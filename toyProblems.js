1f5674c8b1ae44fe7203a8f00d09ab7d8e96484b
Ryan <ryanhca@users.noreply.github.com>
2015-01-13 10:19:26 -0700

    Update toyProblems.js

--- toyProblems.js
+++ toyProblems.js
@@ -1,6 +1,6 @@
 
 
-// Write a function called finder that accepts two parameters, the first one is a strong and the second is an array
+// Write a function called finder that accepts two parameters, the first one is a string and the second is an array
 // have your function return true if the string is found in the array and false if it is not
 var nums = ["Javascript", "hello", "world"];
 
